55b46f840a0ee3b73f1ea7b43149d8bb9e5d624c
tksundar <53440744+tksundar@users.noreply.github.com>
2023-10-08 10:09:34 +0530

    added binary version to bin

---
+++ build.rs
@@ -0,0 +1,14 @@
+use std:: fs;
+use std::path::Path;
+
+fn main() {
+    let out_dir = "target/release";
+    let dest_path = Path::new(&out_dir).join("static");
+    println!("{:?}",dest_path);
+
+    fs::create_dir_all(&dest_path).unwrap();
+    fs::copy("static/index.html", dest_path.join("index.html")).unwrap();
+    fs::copy("static/main.html", dest_path.join("main.html")).unwrap();
+    fs::copy("static/file_upload.html", dest_path.join("file_upload.html")).unwrap();
+    fs::copy("static/market_simulator.html", dest_path.join("market_simulator.html")).unwrap();
+}
